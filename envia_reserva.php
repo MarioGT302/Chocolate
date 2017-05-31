6afb45b86a6889093b9cb8d295bc3664101057fc
MarioGT302 <MarioGT302@hotmail.com>
2017-05-31 09:58:00 -0500

    chocolate
    chocolate

---
+++ envia_reserva.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/Choco.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>envia_reserva</title>
+<!-- InstanceEndEditable -->
+<link href="estilo.css" rel="stylesheet" type="text/css" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="Contenido" type="boolean" value="true" -->
+</head>
+
+<body>
+
+<div class="container">
+  <div class="red_arriba"></div>
+  <div class="header"><!-- end .header -->
+    <h1>TIPOS DE CHOCOLATE  </h1>
+  </div>
+  <div class="sidebar1">
+    <ul class="nav">
+      <li><a href="index.htm">Inicio</a></li>
+      <li><a href="tipos.htm">Tipos</a></li>
+      <li><a href="lugar.htm">Lugares</a></li>
+      <li><a href="galeria.htm">Historia</a></li>
+    </ul>
+    <p>&nbsp;</p>
+    <!-- end .sidebar1 --></div>
+  <div class="content">
+    <div class="content_content1"></div>
+    <h2><!-- InstanceBeginEditable name="Titulo" -->Chocolate<!-- InstanceEndEditable --></h2>
+    <h3><!-- InstanceBeginEditable name="Contenido" --><p>Los chocolates un festin de dioses</p>
+	<?php
+    
+    ?>
+	
+	<!-- InstanceEndEditable --></h3>
+    <p>&nbsp;</p>
+
+  </div>
+  <div class="footer">
+    <p>&nbsp;</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+<!-- InstanceEnd --></html>
